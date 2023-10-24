4e66c0341bfc89db5466f664a5eb162b74208b73
Bismark Agyare <bismarkagyare54@gmail.com>
2023-10-24 13:51:32 +0000

    feat: implement random character selection in Cards component

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import getRandomCharacters from '../utils/randomCharacters';
 
 const Cards = () => {
+  const characters = getRandomCharacters();
+
   return (
     <div className="cards-container">
-      <div>cards go here..</div>
+      <div>
+        {characters.map((obj, index) => (
+          <div key={index}>{obj.name}</div>
+        ))}
+      </div>
     </div>
   );
 };
