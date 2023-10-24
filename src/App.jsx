435c32c8d183f32154c869d9e9129a239d9f5cbb
Bismark Agyare <bismarkagyare54@gmail.com>
2023-10-24 12:38:50 +0000

    chore: Add image and video to assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import backgroundImage from './assets/background.png';
+import backgroundVideo from './assets/background.mp4';
+
 const App = () => {
-  return <div>hello</div>;
+  return (
+    <div className="app">
+      <div>Test</div>
+      <video id="background-vid" autoPlay loop poster={backgroundImage}>
+        <source src={backgroundVideo} type="video/mp4" />
+      </video>
+    </div>
+  );
 };
 
 export default App;
