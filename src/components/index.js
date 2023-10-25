d7bae28be0fa2cf4380bf661e496ed6229ae2eb6
Bismark Agyare <bismarkagyare54@gmail.com>
2023-10-25 15:29:21 +0000

    feat: Add index for exports

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Header } from './Header';
+export { default as Cards } from './Cards';
+export { default as Scoreboard } from './Scoreboard';
+export { default as Footer } from './Footer';
