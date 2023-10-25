60ca03357c4d45b07c12b23c28699104bbb81f0c
Bismark Agyare <bismarkagyare54@gmail.com>
2023-10-25 11:08:12 +0000

    refactor: Refactor card component

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 
-const Cards = ({ characters, onSelection }) => {
-  return (
-    <div className="cards-container">
-      <div>
-        {characters.map((obj, index) => (
-          <div key={index} className="card" onClick={() => onSelection(obj)}>
-            {obj.name}
-          </div>
-        ))}
-      </div>
+const Card = ({ character, onSelection }) => (
+  <div className="card" onClick={() => onSelection(character)}>
+    {character.name}
+  </div>
+);
+
+const Cards = ({ characters, onSelection }) => (
+  <div className="cards-container">
+    <div>
+      {characters.map((character, index) => (
+        <Card key={index} character={character} onSelection={onSelection} />
+      ))}
     </div>
-  );
-};
+  </div>
+);
 
 export default Cards;
