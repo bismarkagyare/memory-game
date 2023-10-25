a913635758aec9e6501013a406b4b556b3316f29
Bismark Agyare <bismarkagyare54@gmail.com>
2023-10-25 13:02:28 +0000

    feat: Create characters array

---
+++ src/utils/charArray.js
@@ -0,0 +1,17 @@
+const characters = [
+  { name: 'Luffy', image: null },
+  { name: 'Nami', image: null },
+  { name: 'Zoro', image: null },
+  { name: 'Usopp', image: null },
+  { name: 'Sanji', image: null },
+  { name: 'Chopper', image: null },
+  { name: 'Robin', image: null },
+  { name: 'Franky', image: null },
+  { name: 'Brook', image: null },
+  { name: 'Jinbe', image: null },
+  { name: 'Vivi', image: null },
+  { name: 'Zeus', image: null },
+  { name: 'Karoo', image: null },
+];
+
+export default characters;
--- src/utils/randomCharacters.js
+++ src/utils/randomCharacters.js
@@ -1,20 +1,20 @@
-const characters = [
-  { name: 'Luffy', image: null },
-  { name: 'Nami', image: null },
-  { name: 'Zoro', image: null },
-  { name: 'Usopp', image: null },
-  { name: 'Sanji', image: null },
-  { name: 'Chopper', image: null },
-  { name: 'Robin', image: null },
-];
+// const characters = [
+//   { name: 'Luffy', image: null },
+//   { name: 'Nami', image: null },
+//   { name: 'Zoro', image: null },
+//   { name: 'Usopp', image: null },
+//   { name: 'Sanji', image: null },
+//   { name: 'Chopper', image: null },
+//   { name: 'Robin', image: null },
+// ];
 
-const getRandomCharacters = () => {
-  // get a shuffled array of characters using fisher-yates shuffle
-  const shuffledCharacters = [...characters].sort(() => Math.random() - 0.5);
+// const getRandomCharacters = () => {
+//   // get a shuffled array of characters using fisher-yates shuffle
+//   const shuffledCharacters = [...characters].sort(() => Math.random() - 0.5);
 
-  // select the first five characters
-  const selectedCharacters = shuffledCharacters.slice(0, 5);
-  return selectedCharacters;
-};
+//   // select the first five characters
+//   const selectedCharacters = shuffledCharacters.slice(0, 5);
+//   return selectedCharacters;
+// };
 
-export default getRandomCharacters;
+// export default getRandomCharacters;
