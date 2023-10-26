0072fbcae1e4f310ebf7d31cf8e83befc00fc6ce
Bismark Agyare <bismarkagyare54@gmail.com>
2023-10-26 18:42:09 +0000

    feat: Add some state & animate cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,20 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Card = ({ character, onSelection }) => (
-  <div className="card" onClick={() => onSelection(character)}>
-    <img src={character.image} alt="" />
-    <p>{character.name}</p>
-  </div>
-);
+// const Card = ({ character, onSelection }) => (
+//   <div className="card" onClick={() => onSelection(character)}>
+//     <img src={character.image} alt="" />
+//     <p>{character.name}</p>
+//   </div>
+// );
 
 const Cards = ({ getRandomChars, onSelection }) => {
-  const randomChars = getRandomChars();
+  const [randomChars, setRandomChars] = useState(getRandomChars());
+  const [canClick, setCanClick] = useState(true);
+  const [firstLoad, setFirstLoad] = useState(true);
+
+  useEffect(() => {
+    //if its not the first load, then execute code
+    if (!firstLoad) {
+      setTimeout(() => {
+        setRandomChars(getRandomChars());
+      }, 1000);
+    }
+  }, [getRandomChars, firstLoad]);
+
+  const handleClick = (character) => {
+    if (!canClick) return;
+
+    setFirstLoad(false);
+    setCanClick(false);
+
+    document.querySelectorAll('.card').forEach((element) =>
+      element.animate(
+        [
+          // keyframes
+          { transform: 'rotateY(0turn)' },
+          { transform: 'rotateY(0.5turn)' },
+        ],
+        {
+          // options
+          duration: 1000,
+          iterations: 2,
+          direction: 'alternate',
+        }
+      )
+    );
+
+    onSelection(character);
+
+    setTimeout(() => {
+      setCanClick(true);
+    }, 2000);
+  };
 
   return (
     <div className="cards-container">
       <div>
         {randomChars.map((character, index) => (
-          <Card key={index} character={character} onSelection={onSelection} />
+          <div
+            key={index}
+            className="card"
+            onClick={() => handleClick(character)}
+          >
+            <img src={character.image} alt="" />
+            <p>{character.name}</p>
+          </div>
         ))}
       </div>
     </div>
